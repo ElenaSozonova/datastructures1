e029bff58280c2e3bd096331ff3dbe0281fce027
ElenaSozonova <sozonova.ea@yandex.ru>
2020-04-02 03:52:35 +0300

    Linkedlist Start

---
+++ datastructures/LinkedList.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace datastructures
+{
+    public class LinkedList
+    {
+        private Node root; // хр ссылка на первый элемент 
+
+        public int Lenght { get; private set; }
+
+        public LinkedList()
+        {
+            root = null;
+            Lenght = 0;
+        }
+
+        public LinkedList(int a)
+        {
+            root = new Node(a);
+            Lenght = 1;
+        }
+
+        public void AddToEnd(int a) //доб в конец 
+        {
+            if (Lenght == 0)//(root==null)
+            {
+                root = new Node(a);
+                Lenght = 1;
+            }
+            else if (Lenght == 1)//(root!=null&&root.Next==null) сам root сущ но никуда не ссылается 
+            {
+                root.Next = new Node(a);
+                Lenght++;
+            }
+            else
+            {
+                Node tmp = root;  // доп переменная = первому элементу 
+                while (tmp.Next != null)
+                {
+                    tmp = tmp.Next;   // tmp.Next внутри лежит ссылка на след объект     
+                }
+                tmp.Next = new Node(a); 
+                Lenght++;
+            }
+        }
+
+        public int[] ReturnArray() //пробр список в массив и возвр его 
+        {
+            int[] array = new int[Lenght]; //созд массив, кот будем возвращать 
+            if (Lenght != 0)
+            {
+                int i = 0;
+                Node tmp = root; //первый элемент 
+                do
+                {
+                    array[i] = tmp.Value;
+                    i++;
+                    tmp = tmp.Next;
+                } while (tmp != null);
+            }
+            return array;
+        }
+
+        public void AddToStart(int a) //доб в начало 
+        {
+            Node tmp = new Node(a); //созд новую ноду 
+            tmp.Next = root; // она ссылается на знач, кот лежит в root 
+            root = tmp; //root теперь ссылается на новую ноду 
+            Lenght++; //увел длину 
+        }
+
+        public void AddToIndex(int a, int index)
+        {
+            
+        }
+    }
+}
--- datastructures/Program.cs
+++ datastructures/Program.cs
@@ -6,7 +6,26 @@ namespace datastructures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LinkedList L = new LinkedList();
+            L.AddToEnd(1);
+            Vivod(L.ReturnArray());
+            L.AddToEnd(2);
+            Vivod(L.ReturnArray());
+            L.AddToEnd(3);
+            Vivod(L.ReturnArray());
+            L.AddToStart(0);
+            Vivod(L.ReturnArray());
+            L.AddToIndex(9, 1);
+            Vivod(L.ReturnArray());
+        }
+
+        static void Vivod(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
